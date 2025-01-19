c55b2d2cfe5e49dea6f73e56f12446e427f7447c
Ivan Brencsics <ivan.brencsics@gmail.com>
2025-01-19 17:54:38 +0100

    0.0.12 - CSRF

---
+++ cmd/web/routes.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/ibrencsics/poc-web/pkg/config"
+	"github.com/ibrencsics/poc-web/pkg/handlers"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Logger)
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/link", handlers.Repo.Link)
+	mux.Post("/link", handlers.Repo.PostLink)
+	mux.Get("/about", handlers.Repo.About)
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
+	return mux
+}
---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,67 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/ibrencsics/poc-web/pkg/config"
+	"github.com/ibrencsics/poc-web/pkg/models"
+	"github.com/ibrencsics/poc-web/pkg/render"
+)
+
+// Repo the repository used by the handlers
+var Repo *Repository
+
+// Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+// Home is the handler for the home page
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	render.RenderTemplate(w, r, "home.page.tmpl", &models.TemplateData{})
+}
+
+// About is the handler for the about page
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	// perform some logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again"
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
+
+	// send data to the template
+	render.RenderTemplate(w, r, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
+
+// Link render
+func (m *Repository) Link(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "link.page.tmpl", &models.TemplateData{})
+}
+
+// PostLink handles post
+func (m *Repository) PostLink(w http.ResponseWriter, r *http.Request) {
+	start := r.Form.Get("start")
+	end := r.Form.Get("end")
+	fmt.Println("start", start)
+
+	w.Write([]byte(fmt.Sprintf("start date is %s and end is %s", start, end)))
+}
